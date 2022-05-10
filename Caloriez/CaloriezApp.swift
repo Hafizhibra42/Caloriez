88afe19fc248f88f9f579b5dafe06dbbe1fa73eb
Hafizh Ibrahim <hafizhibra42@gmail.com>
2022-05-10 23:02:27 +0700

    Initial Commit

---
+++ Caloriez/CaloriezApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriezApp.swift
+//  Caloriez
+//
+//  Created by Hafizh Ibrahim on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CaloriezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
